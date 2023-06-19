3f7bdc47c905fdfbb335b82dab456088d3d5446a
David Lin <13061926+DavidLHW@users.noreply.github.com>
2023-06-19 13:13:15 +0800

    move env -> config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,14 +18,14 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = process.env.SITE || "";
+const LIVE_URL = "https://davidlhw.dev";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCALHOST_URL;
+let SITE_URL = LOCALHOST_URL;
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
-  BASE_URL = LIVE_URL;
+  SITE_URL = LIVE_URL;
 }
 
 // https://astro.build/config
@@ -33,7 +33,7 @@ export default defineConfig({
   server: {
     port: SERVER_PORT,
   },
-  site: BASE_URL,
+  site: SITE_URL,
   integrations: [
     sitemap(),
     tailwind({
