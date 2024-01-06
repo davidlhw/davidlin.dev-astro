9f39ffc9bc420544a945b5c13c498adc52be42cd
David Lin <13061926+DavidLHW@users.noreply.github.com>
2024-01-06 19:59:58 +0800

    build: bump packages to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
-import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
@@ -34,6 +33,7 @@ export default defineConfig({
     port: SERVER_PORT,
   },
   site: SITE_URL,
+  prefetch: true,
   integrations: [
     sitemap(),
     tailwind({
@@ -41,7 +41,6 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    prefetch(),
     partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
